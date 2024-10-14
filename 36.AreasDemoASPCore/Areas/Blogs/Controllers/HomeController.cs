625edc5236037d8815037f186291e0dcb09bc256
VivekSonu <viveksonu37@gmail.com>
2024-10-14 20:04:12 +0530

    10/14/24

---
+++ 36.AreasDemoASPCore/Areas/Blogs/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreasDemoASPCore.Areas.Blogs.Controllers
+{
+    [Area("Blogs")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
